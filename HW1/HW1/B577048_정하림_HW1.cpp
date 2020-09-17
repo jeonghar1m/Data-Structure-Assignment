ed6ba6c09beb0397d54f59c43c5f1f3e68ee6656
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-09-17 21:48:05 +0900

    Update B577048_정하림_HW1.cpp

--- HW1/HW1/B577048_정하림_HW1.cpp
+++ HW1/HW1/B577048_정하림_HW1.cpp
@@ -17,7 +17,7 @@ int main()
 	playlist my_playlist;
 	my_playlist.no_of_songs = 0;
 	int no = my_playlist.no_of_songs;
-	strcpy_s(my_playlist.song_list[0].artist, 10, "Twice");
+	strcpy_s(my_playlist.song_list[0].artist, 10, "123");
 	strcpy_s(my_playlist.song_list[0].title, 10, "Cheer Up!");
 	strcpy_s(my_playlist.song_list[1].artist, 10, "BTS");
 	strcpy_s(my_playlist.song_list[1].title, 10, "Dynamite");
@@ -27,7 +27,7 @@ int main()
 
 	printf("No\tArtist\tTitle\n");
 	printf("==\t======\t=====\n");
-	for (no; no < 3; ++no)
+	for (no; no < 10; ++no)
 		cout << no + 1 << "\t" << my_playlist.song_list->artist[no] << "\t" << my_playlist.song_list->title[no] << endl;
 	cout << "A total number of songs in the list: " << no << endl;
 	return 0;
