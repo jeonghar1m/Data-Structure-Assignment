5367b65affa6a525a9bd57dcf71fb9741ec8489e
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-09-17 21:48:30 +0900

    Update B577048_정하림_HW1.cpp

--- HW1/HW1/B577048_정하림_HW1.cpp
+++ HW1/HW1/B577048_정하림_HW1.cpp
@@ -17,7 +17,7 @@ int main()
 	playlist my_playlist;
 	my_playlist.no_of_songs = 0;
 	int no = my_playlist.no_of_songs;
-	strcpy_s(my_playlist.song_list[0].artist, 10, "123");
+	strcpy_s(my_playlist.song_list[0].artist, 10, "Twice");
 	strcpy_s(my_playlist.song_list[0].title, 10, "Cheer Up!");
 	strcpy_s(my_playlist.song_list[1].artist, 10, "BTS");
 	strcpy_s(my_playlist.song_list[1].title, 10, "Dynamite");
