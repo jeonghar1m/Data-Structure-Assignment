cd7c4d39f75942bb37e6c1884d03292e5b23eee9
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-09-18 16:58:25 +0900

    Update B577048_정하림_HW1.cpp

--- HW1/HW1/B577048_정하림_HW1.cpp
+++ HW1/HW1/B577048_정하림_HW1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 using namespace std;
 typedef struct Song
 {
@@ -9,25 +8,30 @@ typedef struct Song
 typedef struct Playlist
 {
 	int no_of_songs;
-	song song_list[30];
+	song song_list[30][10];
 } playlist;
-
 int main()
 {
 	playlist my_playlist;
+	//song song;
 	my_playlist.no_of_songs = 0;
-	int* no_of_songs = &my_playlist.no_of_songs;
-	strcpy_s(my_playlist.song_list[0].artist, "Twice");
-	strcpy_s(my_playlist.song_list[0].title, "Cheer Up!");
-	strcpy_s(my_playlist.song_list[1].artist, "BTS");
-	strcpy_s(my_playlist.song_list[1].title, "Dynamite");
-	strcpy_s(my_playlist.song_list[2].artist, "IU");
-	strcpy_s(my_playlist.song_list[2].title, "25");
+
+	strcpy_s(my_playlist.song_list[0]->artist, "Twice");
+	strcpy_s(my_playlist.song_list[0]->title, "CheerUp");
+	my_playlist.no_of_songs++;
+	strcpy_s(my_playlist.song_list[0]->artist, "BTS");
+	strcpy_s(my_playlist.song_list[0]->title, "Dynamite");
+	my_playlist.no_of_songs++;
+	strcpy_s(my_playlist.song_list[0]->artist, "IU");
+	strcpy_s(my_playlist.song_list[0]->title, "25");
+	my_playlist.no_of_songs++;
 
 	cout << "No\tArtist\tTitle" << endl;
 	cout << "==\t======\t=====" << endl;
-	for (int i = 0; i <= my_playlist.no_of_songs; ++i)
-		cout << i + 1 << "\t" << my_playlist.song_list->artist[i] << "\t" << my_playlist.song_list->title[i] << endl;
-	cout << "A total number of songs in the list: " << *no_of_songs << endl;
-	return 0;
+	for (int i = 1; i <= my_playlist.no_of_songs; ++i)
+	{
+		//printf("%d\t%s\t%s", i, my_playlist.song_list.artist[i - 1], my_playlist.song_list.title[i - 1]);
+		cout << i << "\t" << my_playlist.song_list[0].artist[i] << "\t" << my_playlist.song_list[0].title[i] << endl;
+	}
+	cout << "A total number of songs in the list: " << my_playlist.no_of_songs << endl;
 }
