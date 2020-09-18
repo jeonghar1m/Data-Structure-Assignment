1848f8fef6905f150a9e222b4f575204e670a2ed
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-09-18 14:00:13 +0900

    Update B577048_정하림_HW1.cpp

--- HW1/HW1/B577048_정하림_HW1.cpp
+++ HW1/HW1/B577048_정하림_HW1.cpp
@@ -24,8 +24,8 @@ int main()
 	strcpy_s(my_playlist.song_list[2].artist, "IU");
 	strcpy_s(my_playlist.song_list[2].title, "25");
 
-	printf("No\tArtist\tTitle\n");
-	printf("==\t======\t=====\n");
+	cout << "No\tArtist\tTitle" << endl;
+	cout << "==\t======\t=====" << endl;
 	for (int i = 0; i <= my_playlist.no_of_songs; ++i)
 		cout << i + 1 << "\t" << my_playlist.song_list->artist[i] << "\t" << my_playlist.song_list->title[i] << endl;
 	cout << "A total number of songs in the list: " << *no_of_songs << endl;
