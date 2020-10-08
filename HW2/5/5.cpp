fd1c7d0a328512b8594536186e12a2a13436c669
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-08 19:09:49 +0900

    create

---
+++ HW2/5/5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int sum(int n)
+{
+	//기존 순환적 구조의 소스코드
+	/*if (n == 1)
+		return 1;
+	else
+		return (n + sum(n - 1));*/
+	int temp = 0;
+	if (n == 1)
+		temp = n;
+	else
+	{
+		for (n; n >= 1; n--)
+			temp += n;
+	}
+	
+	return temp;
+}
+int main()
+{
+	cout << sum(5) << endl;
+}
