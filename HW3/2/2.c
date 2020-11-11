1436184e5e22a064a8fb2c319bcc0e048430bbe0
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-11 21:35:21 +0900

    create

---
+++ HW3/2/2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+	int data;
+	struct node* link;
+} Node;
+
+Node* add(Node* head, int item)
+{
+
+}
+
+Node* delete(Node* head, int item)
+{
+
+}
+
+void clear(Node* head)
+{
+
+}
+
+int is_in_list(Node* head, int item)
+{
+
+}
+
+unsigned int get_length(Node* head)
+{
+
+}
+
+int is_empty(Node* head)
+{
+
+}
+
+void print_list(Node* head)
+{
+
+}
+
+int main()
+{
+	return 0;
+}
