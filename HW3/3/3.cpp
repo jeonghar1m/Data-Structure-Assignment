7e1cb1aa28a7cdf715f11197d1eb09b22d7bee34
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-13 03:13:11 +0900

    Update 3.cpp

--- HW3/3/3.cpp
+++ HW3/3/3.cpp
@@ -47,14 +47,41 @@ int main()
 	DNode* head = NULL;
 
 	//더미 헤드와 더미 테일 노드의 초기화
-	header.data = NULL;
+	header.data = NULL;		// 초기화하지만 사용하지는 않는 데이터
 	header.llink = NULL;
 	header.rlink = &trailer;
 
-	trailer.data = NULL;
+	trailer.data = NULL;	// 초기화하지만 사용하지는 않는 데이터
 	trailer.llink = &header;
 	trailer.rlink = NULL;
 
 	//리스트의 초기화
 	head = &header;
+
+	int menu_select = 0;
+
+	while (menu_select != 4)
+	{
+		printf("메뉴를 선택해주세요. 1. 앱 실행, 2. 앱 종료, 3. 실행 중 앱 출력, 4. 프로그램 종료\n");
+		scanf("%d", &menu_select);
+
+		switch (menu_select)
+		{
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		case 4:
+			return 0;
+		}
+	}
+	return 0;
 }
