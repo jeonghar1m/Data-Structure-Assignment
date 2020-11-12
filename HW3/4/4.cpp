44bd90e4901547dc831b078b8408725880ad1f03
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-13 06:27:45 +0900

    complete

--- HW3/4/4.cpp
+++ HW3/4/4.cpp
@@ -1,12 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-struct Stack
+#define TRUE 1
+#define FALSE 0
+#define MAX_STACK_SIZE 200000
+
+typedef int element;
+
+typedef struct Stack
 {
+	element data[MAX_STACK_SIZE] = {};
+	int top;
+} stack;
 
-};
-clock_t start, finish;
-double duration;
-int i, x;
+void initialize(stack *s)
+{
+	s->top = -1;
+}
 
-Stack s;
+int is_empty(stack* s)
+{
+	if (s->top == -1)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+int is_full(stack* s)
+{
+	if (s->top == (MAX_STACK_SIZE - 1))
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void push(stack* s, element item)
+{
+	if (is_full(s))
+	{
+		fprintf(stderr, "Stack is full!\n");
+		return;
+	}
+	else
+	{
+		(s->top)++;
+		s->data[s->top] = item;
+	}
+}
+
+element pop(stack* s)
+{
+	element temp;
+
+	if (is_empty(s))
+	{
+		fprintf(stderr, "Stack is empty!\n");
+		exit(1);
+	}
+	else
+	{
+		temp = s->data[s->top];
+		(s->top)--;
+		return temp;
+	}
+}
+
+int main()
+{
+	clock_t start, finish;
+	double duration;
+	int i;
+
+	Stack s;
+
+	initialize(&s);
+
+	start = clock();
+
+	for (i = 0; i < MAX_STACK_SIZE; i++)
+	{
+		push(&s, 1);
+		pop(&s);
+	}
+	finish = clock();
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("%lf초입니다.\n", duration);
+
+	return 0;
+}
